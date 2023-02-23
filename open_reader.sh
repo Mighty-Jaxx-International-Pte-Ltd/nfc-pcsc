e50dc09d865d3e487c13c0fa6d2ed830ee9822dd
gibtang <gibtang@gmail.com>
2023-02-23 15:35:23 +0800

    updated script

--- open_reader.sh
+++ open_reader.sh
@@ -1,2 +1,8 @@
-#
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+cd $SCRIPT_DIR
+echo 'directory changed'
 npm run st-reader
+read
